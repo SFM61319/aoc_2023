d84941ddfa65d20f51c7949de5116f056175b0d6
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2023-12-01 12:00:26 +0530

    Solve day 1 - part 1 using iterators

---
+++ src/days/day1.rs
@@ -0,0 +1,30 @@
+#[aoc_runner_derive::aoc(day1, part1, Iterators)]
+pub fn solve_part1_iterators(input: &str) -> u32 {
+    const ZERO: u8 = b'0';
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = line
+                .bytes()
+                .filter(|ch| ch.is_ascii_digit())
+                .map(|ch| ch - ZERO);
+
+            let digit1 = digits.next().unwrap();
+            let digit2 = digits.last().unwrap_or(digit1);
+            let digits = digit1 * 10 + digit2;
+
+            digits as u32
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solve_part1_iterators_sample() {
+        assert_eq!(
+            super::solve_part1_iterators("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"),
+            142
+        )
+    }
+}
