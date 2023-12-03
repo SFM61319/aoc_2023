fe6d5e9faf920016eb23a3caf7be706b92d274e8
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2023-12-03 21:53:03 +0530

    Solve day 3 - part 2 (it just works)
    If it ain't broke, don't fix it (actually, optimize it)

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 fn get_empty_parts(part_count: usize) -> String {
     let mut empty_parts = String::with_capacity(part_count);
     for _ in usize::MIN..part_count {
@@ -23,7 +25,7 @@ fn get_part_numbers(lines: &[&str], line_no: usize, curr_line: &str) -> u32 {
 
     let next_line = get_line_or(lines, line_no + 1, &empty_parts);
     let prev_line = if line_no == usize::MIN {
-        &empty_parts.as_bytes()
+        empty_parts.as_bytes()
     } else {
         get_line_or(lines, line_no - 1, &empty_parts)
     };
@@ -113,6 +115,91 @@ pub fn solve_part1(input: &str) -> u32 {
         .sum()
 }
 
+type Coords = (usize, usize);
+
+fn get_gears(lines: &[&str]) -> HashMap<Coords, Vec<u32>> {
+    const GEAR: u8 = b'*';
+    let mut gears = HashMap::<Coords, Vec<u32>>::new();
+
+    for (line_no, &curr_line) in lines.iter().enumerate() {
+        let curr_line = curr_line.as_bytes();
+        let empty_parts = get_empty_parts(curr_line.len());
+
+        let next_line = get_line_or(lines, line_no + 1, &empty_parts);
+        let prev_line = if line_no == usize::MIN {
+            empty_parts.as_bytes()
+        } else {
+            get_line_or(lines, line_no - 1, &empty_parts)
+        };
+
+        let mut curr_number = u32::MIN;
+        let mut surrounding_gears = HashSet::<Coords>::new();
+
+        for (i, ch) in curr_line.iter().enumerate() {
+            if ch.is_ascii_digit() {
+                curr_number = curr_number * 10u32 + (ch - b'0') as u32;
+                if let Some(&GEAR) = prev_line.get(i) {
+                    surrounding_gears.insert((line_no - 1, i));
+                }
+                if let Some(&GEAR) = prev_line.get(i + 1) {
+                    surrounding_gears.insert((line_no - 1, i + 1));
+                }
+                if let Some(&GEAR) = curr_line.get(i + 1) {
+                    surrounding_gears.insert((line_no, i + 1));
+                }
+                if let Some(&GEAR) = next_line.get(i + 1) {
+                    surrounding_gears.insert((line_no + 1, i + 1));
+                }
+                if let Some(&GEAR) = next_line.get(i) {
+                    surrounding_gears.insert((line_no + 1, i));
+                }
+                if i > usize::MIN {
+                    if let Some(&GEAR) = next_line.get(i - 1) {
+                        surrounding_gears.insert((line_no + 1, i - 1));
+                    }
+                    if let Some(&GEAR) = curr_line.get(i - 1) {
+                        surrounding_gears.insert((line_no, i - 1));
+                    }
+                    if let Some(&GEAR) = prev_line.get(i - 1) {
+                        surrounding_gears.insert((line_no - 1, i - 1));
+                    }
+                }
+            } else {
+                for &surrounding_gear in &surrounding_gears {
+                    if !gears.contains_key(&surrounding_gear) {
+                        gears.insert(surrounding_gear, Vec::new());
+                    }
+
+                    gears.get_mut(&surrounding_gear).unwrap().push(curr_number);
+                }
+
+                curr_number = u32::MIN;
+                surrounding_gears.clear();
+            }
+        }
+
+        for &surrounding_gear in &surrounding_gears {
+            if !gears.contains_key(&surrounding_gear) {
+                gears.insert(surrounding_gear, Vec::new());
+            }
+
+            gears.get_mut(&surrounding_gear).unwrap().push(curr_number);
+        }
+    }
+
+    gears
+}
+
+#[aoc_runner_derive::aoc(day3, part2)]
+pub fn solve_part2(input: &str) -> u32 {
+    let lines = input.lines().collect::<Vec<&str>>();
+    get_gears(&lines)
+        .values()
+        .filter(|&v| v.len() == 2)
+        .map(|v| v[0] * v[1])
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -128,5 +215,29 @@ mod tests {
 ...$.*....
 .664.598..";
         assert_eq!(super::solve_part1(input), 4361)
+    }
+
+    #[test]
+    fn test_solve_part2_sample() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!(super::solve_part2(input), 467835)
+    }
+
+    #[test]
+    fn test_solve_part2_edge() {
+        let input = ".664.598..
+.111..755.
+...*..755.
+.111..755.";
+        assert_eq!(super::solve_part2(input), 12321);
     }
 }
