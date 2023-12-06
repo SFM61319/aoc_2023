67ee93474425ad431acac02f157eecf4ef29705d
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2023-12-06 12:52:52 +0530

    Solve day 6 - part 2

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,17 +1,17 @@
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct Race {
-    allowed_time: u32,
-    distance_record: u32,
+    allowed_time: u64,
+    distance_record: u64,
 }
 
 impl Race {
     #[inline]
     pub const fn new() -> Self {
-        Self::with(u32::MIN, u32::MIN)
+        Self::with(u64::MIN, u64::MIN)
     }
 
     #[inline]
-    pub const fn with(allowed_time: u32, distance_record: u32) -> Self {
+    pub const fn with(allowed_time: u64, distance_record: u64) -> Self {
         Self {
             allowed_time,
             distance_record,
@@ -19,16 +19,16 @@ impl Race {
     }
 
     #[inline]
-    pub fn get_number_of_ways_to_win(&self) -> u32 {
-        let hold_times = u32::MIN..=self.allowed_time;
+    pub fn get_number_of_ways_to_win(&self) -> u64 {
+        let hold_times = u64::MIN..=self.allowed_time;
         hold_times
             .map(|hold_time| self.get_distance_after(hold_time))
             .filter(|&distance| self.can_win(distance))
-            .count() as u32
+            .count() as u64
     }
 
     #[inline]
-    const fn get_distance_after(&self, hold_time: u32) -> u32 {
+    const fn get_distance_after(&self, hold_time: u64) -> u64 {
         let speed = hold_time;
         let remaining_time = self.allowed_time - hold_time;
 
@@ -36,7 +36,7 @@ impl Race {
     }
 
     #[inline]
-    const fn can_win(&self, distance: u32) -> bool {
+    const fn can_win(&self, distance: u64) -> bool {
         distance > self.distance_record
     }
 }
@@ -47,15 +47,14 @@ fn filter_numbers(numbers: &str) -> &str {
 }
 
 #[inline]
-fn parse_numbers(numbers: &str) -> impl Iterator<Item = u32> + '_ {
+fn parse_numbers(numbers: &str) -> impl Iterator<Item = u64> + '_ {
     filter_numbers(numbers)
         .split_ascii_whitespace()
         .map(|num| num.parse().unwrap())
 }
 
-#[inline]
 #[aoc_runner_derive::aoc(day6, part1)]
-pub fn solve_part1(input: &str) -> u32 {
+pub fn solve_part1(input: &str) -> u64 {
     let mut input = input.lines();
 
     let allowed_times = input.next().unwrap();
@@ -73,6 +72,25 @@ pub fn solve_part1(input: &str) -> u32 {
         .product()
 }
 
+#[inline]
+fn parse_number(numbers: &str) -> u64 {
+    filter_numbers(numbers).replace(' ', "").parse().unwrap()
+}
+
+#[aoc_runner_derive::aoc(day6, part2)]
+pub fn solve_part2(input: &str) -> u64 {
+    let mut input = input.lines();
+
+    let allowed_time = input.next().unwrap();
+    let distance_record = input.next().unwrap();
+
+    let allowed_time = parse_number(allowed_time);
+    let distance_record = parse_number(distance_record);
+
+    let race = Race::with(allowed_time, distance_record);
+    race.get_number_of_ways_to_win()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -80,5 +98,12 @@ mod tests {
         let input = "Time:      7  15   30
 Distance:  9  40  200";
         assert_eq!(super::solve_part1(input), 288)
+    }
+
+    #[test]
+    fn test_solve_part2_sample() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!(super::solve_part2(input), 71503)
     }
 }
