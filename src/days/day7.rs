afd5243ff70f93ad0416b15fb74306e0ad1c65f5
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2023-12-07 16:02:06 +0530

    Optimize day 7 - part 1
    - Replace hash maps with fixed-size arrays
    - Replace enums with bytes

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,120 +1,62 @@
 use std::cmp::Ordering;
 
-use ahash::AHashMap;
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Card {
-    Two = 2,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    T,
-    J,
-    Q,
-    K,
-    A,
+#[inline]
+fn card_cmp(card_a: u8, card_b: u8, ordering: &[u8; 35]) -> Ordering {
+    ordering[card_a as usize].cmp(&ordering[card_b as usize])
 }
 
-impl Card {
-    pub const fn parse(ch: u8) -> Self {
-        match ch {
-            b'2' => Self::Two,
-            b'3' => Self::Three,
-            b'4' => Self::Four,
-            b'5' => Self::Five,
-            b'6' => Self::Six,
-            b'7' => Self::Seven,
-            b'8' => Self::Eight,
-            b'9' => Self::Nine,
-            b'T' => Self::T,
-            b'J' => Self::J,
-            b'Q' => Self::Q,
-            b'K' => Self::K,
-            b'A' => Self::A,
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub enum HandType {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
-}
-
-impl HandType {
-    pub fn parse(s: &str) -> Self {
-        let mut card_count = AHashMap::<Card, u64>::new();
-        for ch in s.bytes() {
-            let card = Card::parse(ch);
-            card_count
-                .entry(card)
-                .and_modify(|bid| *bid += 1)
-                .or_insert(1);
-        }
-
-        let card_count = card_count.into_values().collect::<Vec<_>>();
-        Self::from_card_count(card_count)
-    }
-
-    fn from_card_count(mut card_count: Vec<u64>) -> Self {
-        card_count.sort();
-        match card_count.as_slice() {
-            [1, 1, 1, 1, 1] => Self::HighCard,
-            [1, 1, 1, 2] => Self::OnePair,
-            [1, 2, 2] => Self::TwoPair,
-            [1, 1, 3] => Self::ThreeOfAKind,
-            [2, 3] => Self::FullHouse,
-            [1, 4] => Self::FourOfAKind,
-            [5] => Self::FiveOfAKind,
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub struct HandBidPair<'a>(HandType, &'a str, u64);
-
-impl<'a> HandBidPair<'a> {
-    pub fn parse(s: &'a str) -> Self {
-        let (hand, bid) = s.trim().split_once(' ').unwrap();
-        let hand_type = HandType::parse(hand);
-        let bid = bid.parse().unwrap();
-
-        Self(hand_type, hand, bid)
-    }
-}
-
-fn hand_cmp(hand_a: &str, hand_b: &str) -> Ordering {
+#[inline]
+fn hand_cmp(hand_a: &str, hand_b: &str, card_ordering: &[u8; 35]) -> Ordering {
     hand_a
         .bytes()
         .zip(hand_b.bytes())
         .fold(Ordering::Equal, |ordering, (card_a, card_b)| {
-            let card_a = Card::parse(card_a);
-            let card_b = Card::parse(card_b);
-
-            ordering.then(card_a.cmp(&card_b))
+            ordering.then_with(|| card_cmp(card_a - b'2', card_b - b'2', card_ordering))
         })
+}
+
+#[inline]
+fn hand_bid_pair_cmp(
+    pair_a: &([u8; 35], &str, u64),
+    pair_b: &([u8; 35], &str, u64),
+    card_ordering: &[u8; 35],
+) -> Ordering {
+    pair_a
+        .0
+        .cmp(&pair_b.0)
+        .then_with(|| hand_cmp(pair_a.1, pair_b.1, card_ordering))
 }
 
 #[aoc_runner_derive::aoc(day7, part1)]
 pub fn solve_part1(input: &str) -> u64 {
-    let mut hand_bid_pairs = input.lines().map(HandBidPair::parse).collect::<Vec<_>>();
+    const ORDERING: [u8; 35] = [
+        1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 0, 0, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 10, 12, 0, 0, 0,
+        0, 0, 11, 0, 0, 9,
+    ];
+    let mut hand_bid_pairs = input
+        .lines()
+        .map(|line| {
+            let (hand, bid) = line.trim().split_once(' ').unwrap();
+            let bid = bid.parse().unwrap();
 
-    hand_bid_pairs.sort_by(|a, b| a.0.cmp(&b.0).then(hand_cmp(a.1, b.1)));
+            let mut card_count = [u8::MIN; 35];
+            for ch in hand.bytes() {
+                let i = (ch - b'2') as usize;
+                card_count[i] += 1;
+            }
+
+            card_count.sort();
+            card_count.reverse();
+
+            (card_count, hand, bid)
+        })
+        .collect::<Vec<([u8; 35], &str, u64)>>();
+
+    hand_bid_pairs.sort_by(|pair_a, pair_b| hand_bid_pair_cmp(pair_a, pair_b, &ORDERING));
     hand_bid_pairs
         .into_iter()
         .enumerate()
-        .map(|(rank, hand_bid_pair)| hand_bid_pair.2 * (1 + rank as u64))
+        .map(|(rank, (_, _, bid))| bid * (1 + rank as u64))
         .sum()
 }
 
